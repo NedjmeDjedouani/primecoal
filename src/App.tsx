cb7404f00dfe489d5a0eccaa2061c276b44b8eaa
Abir zch <zchabir3@gmail.com>
2024-10-04 21:58:51 +0100

    pages 3  et 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import BecomeDistrubutorPage from "./BecomeDistrubutorPage";
+import News from "./News";
+import About from "./About";
 
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route Component={Home} path="/" /> {/* 👈 Renders at /app/ */}
         <Route Component={BecomeDistrubutorPage} path="/becomedis"/>
+        <Route Component={News} path="/news"></Route>
+        <Route Component={About} path="/About"></Route>
       </Routes>
     </BrowserRouter>
   );
