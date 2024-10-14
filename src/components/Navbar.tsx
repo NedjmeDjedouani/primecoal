dd13157c8a4d5517909aa22c5419a74b80b6df35
Nedjme <nejmdje@gmail.com>
2024-10-14 21:35:30 +0100

    hotfix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,23 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-       <ul className="flex  w-full justify-between  max-md:hidden ">
-        <li>
-         <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/news">News</Link>
-        </li>
-        <li>
-          <Link to="/become-distributor">Become a distributor</Link>
-        </li>
-        <li>
-          <Link to="mailto:primecoal@info.com">
-          Contact Us</Link>
-        </li>
-        <li>
-          <Link to="/About">About Us</Link>
-        </li>
-      </ul>
-    
-    );
+  return (
+    <ul className="flex  w-full justify-between  max-md:hidden ">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/news">News</Link>
+      </li>
+      <li>
+        <Link to="/become-distributor">Become a distributor</Link>
+      </li>
+      <li>
+        <Link to="mailto:primecoal@info.com">Contact Us</Link>
+      </li>
+      <li>
+        <Link to="/about-us">About Us</Link>
+      </li>
+    </ul>
+  );
 }
-
-
-
